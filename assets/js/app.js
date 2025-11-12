69b43d22ebde3c4f6d65bada5921e5d6adc5e3bb
TomDlv. <92630186+Tomdlve@users.noreply.github.com>
2025-11-12 11:38:25 +0100

    MAJ des liens

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
 async function fetchAndMergeData() {
   // je mets la liste de mes fichiers (un par trimestre)
   const trimestreFiles = [
-    { file: '/data/nb_validation_Q1_2024.json', idField: 'ida', annee: '2024', trimestre: '1erTrimestre' },
-    { file: '/data/nb_validation_Q2_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '2emeTrimestre' },
-    { file: '/data/nb_validation_Q3_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '3emeTrimestre' },
-    { file: '/data/nb_validation_Q4_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '4emeTrimestre' },
-    { file: '/data/nb_validation_Q1_2025.json', idField: 'ida', annee: '2025', trimestre: '1erTrimestre' },
-    { file: '/data/nb_validation_Q2_2025.json', idField: 'id_zdc', annee: '2025', trimestre: '2emeTrimestre' }
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q1_2024.json', idField: 'ida', annee: '2024', trimestre: '1erTrimestre' },
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q2_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '2emeTrimestre' },
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q3_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '3emeTrimestre' },
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q4_2024.json', idField: 'id_zdc', annee: '2024', trimestre: '4emeTrimestre' },
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q1_2025.json', idField: 'ida', annee: '2025', trimestre: '1erTrimestre' },
+    { file: 'https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/nb_validation_Q2_2025.json', idField: 'id_zdc', annee: '2025', trimestre: '2emeTrimestre' }
   ];
 
   // je vais chercher le fichier sur les informations des gares 
-  const resGares = await fetch('/data/info_gares.json');
+  const resGares = await fetch('https://raw.githubusercontent.com/leolesimple/dataTchoo/main/data/info_gares.json');
   const dataGares = await resGares.json();
 
   // ici je fais un objet ou je range les gares avec leur id
@@ -96,7 +96,7 @@ infos: g
     }
   }
 
-console.log(JSON.stringify(resultat, null, 2)); // J'affiche le résultat dans la console, je mets les paramètres
+//console.log(JSON.stringify(resultat, null, 2)); // J'affiche le résultat dans la console, je mets les paramètres
   // null : pour ne pas filtrer 
   // 2 : pour avoir une indentation de 2 espaces
   return resultat;
@@ -150,7 +150,7 @@ async function afficheTop5() {
       // ajout svg
       const imgHtml = `
         <div class="imgContainer">
-          <img src="./assets/img/handmade_img/classement/${placeNumber}.svg" alt="Gare numéro ${placeNumber} — ${gare.nom}">
+          <img src="https://raw.githubusercontent.com/leolesimple/dataTchoo/main/assets/img/handmade_img/classement/${placeNumber}.svg" alt="Gare numéro ${placeNumber} — ${gare.nom}">
         </div>
       `;
 
@@ -160,3 +160,4 @@ async function afficheTop5() {
 }}
 // Lancer l'affichage
 afficheTop5();
+
